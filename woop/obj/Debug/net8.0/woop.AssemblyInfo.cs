cf5dd6911f9dac92f1983b794e07cbfb151b00c5
vaieldep <denizk4r@gmail.com>
2024-03-21 13:29:34 +0300

    lab4 branch created and web app designed

--- woop/obj/Debug/net8.0/woop.AssemblyInfo.cs
+++ woop/obj/Debug/net8.0/woop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("woop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2100a44e3cc8e049cf006840775387ee0e2c2f4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a28ee296b78515428f0889058156873921b401e")]
 [assembly: System.Reflection.AssemblyProductAttribute("woop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("woop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
